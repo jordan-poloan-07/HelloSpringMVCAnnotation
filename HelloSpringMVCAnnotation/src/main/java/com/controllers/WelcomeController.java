5589177669d1e9f2290da81be040f39ba4ccbc14
jordan-poloan-07 <jordan.poloan@gmail.com>
2014-07-31 12:28:26 +0800

    Added printTimestamped

--- HelloSpringMVCAnnotation/src/main/java/com/controllers/WelcomeController.java
+++ HelloSpringMVCAnnotation/src/main/java/com/controllers/WelcomeController.java
@@ -1,10 +1,15 @@
 package com.controllers;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.models.WelcomeModel;
 
@@ -27,6 +32,19 @@ public class WelcomeController {
 	public String printVariable(@PathVariable String variable, ModelMap model) {
 
 		model.addAttribute("message", "The path is ".concat(variable));
+
+		return "page";
+	}
+
+	@RequestMapping(value = "/timestamped", method = RequestMethod.GET)
+	public String printTimestamped(
+			@RequestParam("startValidity") @DateTimeFormat(pattern = "yyyy-MM-dd'T'hh:mm:ss") Date startValidity,
+			@RequestParam("endValidity") @DateTimeFormat(pattern = "yyyy-MM-dd'T'hh:mm:ss") Date endValidity, ModelMap model) {
+
+		Timestamp ts = new Timestamp(startValidity.getTime());
+		Timestamp ts1 = new Timestamp(endValidity.getTime());
+		
+		model.addAttribute("message", "Okay received timestamps");
 
 		return "page";
 	}
--- HelloSpringMVCAnnotation/src/main/webapp/pages/page.jsp
+++ HelloSpringMVCAnnotation/src/main/webapp/pages/page.jsp
@@ -1,14 +1,15 @@
 <?xml version="1.0" encoding="ISO-8859-1" ?>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
 <title>Insert title here</title>
 </head>
 <body>
 
-<h2> ${message} </h2>
+	<h2>${message}</h2>
 
 </body>
 </html>
